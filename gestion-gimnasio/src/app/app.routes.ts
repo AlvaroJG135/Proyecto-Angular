4991076535f770d978df83e548ef94b5a7c8c455
AlvaroJG135 <alvarojimenez135@gmail.com>
2025-11-23 23:38:32 +0100

    primera ruta cambiada

--- gestion-gimnasio/src/app/app.routes.ts
+++ gestion-gimnasio/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { MaquinaDetalle }  from './maquina-detalle/maquina-detalle';
 import { Registro } from './registro/registro';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     { path: 'maquinas', component: Maquinas },
     { path: 'monitores', component: Monitores },
     { path: 'dashboard', component: Dashboard},
