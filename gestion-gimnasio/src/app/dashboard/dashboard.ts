1493b9ecb011fb7f00c0c58bff7bac6f1d2af1bc
AlvaroJG135 <alvarojimenez135@gmail.com>
2025-11-26 17:35:02 +0100

    creado el dashboard

--- gestion-gimnasio/src/app/dashboard/dashboard.ts
+++ gestion-gimnasio/src/app/dashboard/dashboard.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Maquina } from '../maquina';
+import { Monitor } from '../monitor';
 import { GestionarMaquinas } from '../servicios/gestionar-maquinas';
+import { GestionarMonitores } from '../servicios/gestionar-monitores';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -11,15 +13,31 @@ import { RouterLink } from '@angular/router';
 })
 export class Dashboard {
   maquinas: Maquina[] = [];
+  maquinasTop: Maquina[] = [];
+  monitoresTop: Monitor[] = [];
 
-  constructor(private gestionarMaquinasService: GestionarMaquinas) { }
+  constructor(private gestionarMaquinasService: GestionarMaquinas, private gestionarMonitores: GestionarMonitores) { }
 
   ngOnInit() {
     this.getMaquinas();
+    this.getMonitores();
   }
 
   getMaquinas(): void {
     this.gestionarMaquinasService.getMaquinas()
-      .subscribe(maquinas => this.maquinas = maquinas.slice(1, 5));
+      .subscribe(maquinas => {
+        this.maquinas = maquinas || [];
+        this.maquinasTop = [...this.maquinas]
+          .sort((a, b) => (b.precio || 0) - (a.precio || 0))
+          .slice(0, 3);
+      });
+  }
+
+  getMonitores(): void {
+    this.gestionarMonitores.getMonitores()
+      .subscribe(monitores => {
+        const lista = monitores || [];
+        this.monitoresTop = lista.slice(0, 4);
+      });
   }
 }
